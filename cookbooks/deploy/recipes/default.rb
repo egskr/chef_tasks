cea0d7bc8bdd3ca6fb19ce09ace251005e08bbc0
Student <student@epbyminw2033.minsk.epam.com>
2017-04-07 04:03:57 +0300

    d

---
+++ cookbooks/deploy/recipes/default.rb
@@ -0,0 +1,24 @@
+# Cookbook:: deploy
+# Recipe:: default
+#
+# Copyright:: 2017, The Author Yegor, All Rights Reserved.
+
+remote_file node[:jboss][:zip_app] do
+  source node[:jboss][:app_url]
+  owner node[:jboss][:user]
+  group node[:jboss][:group]
+  action :create_if_missing
+end
+
+package 'unzip' do
+  action :install
+end
+
+execute 'unarchive_app' do
+  command "unzip #{node[:jboss][:zip_app]} -d #{node[:jboss][:jboss_home]}/standalone/deployments/"
+  not_if { ::File.directory? ("#{node[:jboss][:jboss_home]}/standalone/deployments/testweb/") }
+end
+
+execute 'change owner for app' do
+  command "chown -R #{node[:jboss][:user]}:#{node[:jboss][:group]} #{node[:jboss][:jboss_home]}/standalone/deployments/testweb/"
+end
